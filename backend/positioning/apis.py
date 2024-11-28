bbc21570ecd6c7738a2c5c08c3061aa071b591b8
Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>
2024-11-28 14:02:53 +0100

    Added additional logging of received messages

--- backend/positioning/apis.py
+++ backend/positioning/apis.py
@@ -1,11 +1,14 @@
+"""Main API routes"""
+
 import logging
+from typing import Any
 import jsonschema
 from fastapi import APIRouter, Security, Body, HTTPException, status
-from typing import Any
+
 from auth import get_api_key
-from processing.constants import *
 from data.database import synchronize
 from schema_gen.position import Position
+from processing.constants import ENDPOINT_KEY, ENDPOINT_ID_TTN, ENDPOINT_ID_ONBOARD
 from processing.infrastructure import process_position
 from processing.ttn import handle_ttn_message
 from processing.notifier import _initial_data
@@ -18,13 +21,17 @@ logger = logging.getLogger("app.api")
 
 @router.get("/hello")
 async def hello_world():
+    """Just for testing availability"""
     return {"hello": "world"}
 
 
 ## Route: Tracking
 
 @router.post("/tracking/ttn")
-async def raw_tracker(body: Any = Body(), api_key: str = Security(get_api_key)):
+async def raw_tracker(body: Any = Body(), _api_key: str = Security(get_api_key)):
+    """Handles messages directly from the TTN network"""
+    logger.info("Received message on '/tracking/ttn' endpoint: %s", str(body))
+
     try:
         pos = handle_ttn_message(body)
     except jsonschema.exceptions.ValidationError as ve:  # type: ignore
@@ -57,7 +64,10 @@ async def raw_tracker(body: Any = Body(), api_key: str = Security(get_api_key)):
 
 
 @router.post("/tracking/onboard")
-async def onboard_tracker(pos: Position, api_key: str = Security(get_api_key)):
+async def onboard_tracker(pos: Position, _api_key: str = Security(get_api_key)):
+    """Handles messages from clients adhering to the Position API scheme"""
+    logger.info("Received message on '/tracking/onboard' endpoint: %s", str(pos))
+
     # Mark endpoint
     if pos.additions is None:
         pos.additions = {}
@@ -71,11 +81,12 @@ async def onboard_tracker(pos: Position, api_key: str = Security(get_api_key)):
     _last_received = pos
 
 
-_last_received = None  # FIXME Just for testing
+# FIXME Just for testing
+_last_received = None
 
 
 @router.get("/tracking/test/latest")
-async def latest_data(api_key: str = Security(get_api_key)):
+async def latest_data(_api_key: str = Security(get_api_key)):
     """FIXME Just for testing"""
     return _last_received
 
@@ -90,6 +101,7 @@ async def current_data():
 
 
 @router.get("/admin/sync")
-async def synchronize_db(api_key: str = Security(get_api_key)):
+async def synchronize_db(_api_key: str = Security(get_api_key)):
+    """Internal endpoint for triggering synchronization with db"""
     await synchronize()
     return {"synchronized": True}
